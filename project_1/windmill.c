903cd5ed2f07a7c979bbb26a288b82c419545b6f
duan-jia <duanjiabingcom@163.com>
2021-03-09 18:20:41 +0800

    daily update

--- project_1/windmill.c
+++ project_1/windmill.c
@@ -4,90 +4,100 @@
 
 #define     RGB_LED_HIGH    (HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_SET))
 #define     RGB_LED_LOW     (HAL_GPIO_WritePin(LED1_GPIO_Port, LED1_Pin, GPIO_PIN_RESET))
+#define     SPEED1 500
+#define     SPEED2 1000 //速度未知
+#define     SPEED3 0
 
 
 
-void windmill_Process(void)
-{
-	
-
-
-
-}
 /*************************order-control***************************/
-uint8_t queue[20], st, ed, queue_num, color_id;
+uint8_t queue[20], st, ed, queue_num;
+
 uint8_t queue_get(void)
 {
-	if(!queue_num) return 1;
-	if(queue_num == 1)	return queue[st];
-	if(queue_num > 1) {
-	 st ++;
- 	 queue_num --;
-	 if(st > 19) st -= 20;
-	 return queue[st];
-	}
+		if(!queue_num) return 1;
+		if(queue_num == 1)	return queue[st];
+		if(queue_num > 1) 
+		{
+			st ++;
+			queue_num --;
+		  if(st > 19) st -= 20;
+	    return queue[st];
+	  }
 }
+
 void queue_insert(uint8_t control_id)
 {
- queue[ed] = control_id;
- ed ++;
- if(ed > 19) ed -= 20;
- queue_num++;
+   queue[ed] = control_id;
+   ed ++;
+   if(ed > 19) ed -= 20;
+   queue_num++;
 }
 
+void queue_clear(void)
+{
+  st = ed = queue_num = 0;
+}
 
+void Run_led(void)
+{
+	color_id?RGB_LED_Write_24Bits(0x20,0X7F,0X00):RGB_LED_Write_24Bits(0x60,0X00,0X43);
+	//控制灯的流动效果，以后再做
+}
 
 
 
 
 /*************************WS2812——LED***************************/
+void delay_us(uint16_t n)
+{
+	  while(n) n--;
+}
+
 void RGB_LED_Write0(void)
 {
 	 RGB_LED_HIGH;
-	 HAL_Delay(6);
+	 delay_us(6);
    RGB_LED_LOW;
-	 HAL_Delay(21);
+	 delay_us(21);
 }
 
 void RGB_LED_Write1(void)
 {
 	 RGB_LED_HIGH;
-	 HAL_Delay(25);
-   RGB_LED_LOW;
-	 HAL_Delay(3);
+	 delay_us(25);
+	 RGB_LED_LOW;
+	 delay_us(3);
 }
 
 void RGB_LED_Write_Byte(uint8_t byte)
 {
-    uint8_t i;
-   	 // irq_close();
-    for(i=0;i<8;i++)
-        {
-            if(byte&0x80)
-            {
-                    RGB_LED_Write1();
-            }
-            else
-            {
-                    RGB_LED_Write0();
-            }
-        byte <<= 1;
-    }
-	//irq_restore();
+	uint8_t i;
+	for(i=0;i<8;i++)
+			{
+					if(byte&0x80)
+					{
+									RGB_LED_Write1();
+					}
+					else
+					{
+									RGB_LED_Write0();
+					}
+			byte <<= 1;
+	}
 }
 
 void RGB_LED_Write_24Bits(uint8_t green,uint8_t red,uint8_t blue)//GRB......
 {
-//	  irq_close();
-    RGB_LED_Write_Byte(green);
-    RGB_LED_Write_Byte(red);
-    RGB_LED_Write_Byte(blue);
-//		irq_restore();
+	RGB_LED_Write_Byte(green);
+	RGB_LED_Write_Byte(red);
+	RGB_LED_Write_Byte(blue);
 }
 
 void Reset(void)
 {
-	RGB_LED_LOW;HAL_Delay(2900);
+	RGB_LED_LOW;
+	delay_us(2900);
 }
 /*************************motor***************************/
 
@@ -95,40 +105,59 @@ move_t move = {0};
 
 void MOVE_pidinit(void)
 {
-  
-    pid_init_increment(&move.M3508.PidSpeed,4,0.005,0.004,9999);//pid为整定
-
+	pid_init_increment(&move.M3508.PidSpeed,4,0.005,0.004,9999);//pid为整定
 }
 
 
 void MOVE_GetMoveData()
 {
-
+	move.M3508.Rx.Speed = Tar_speed;
 }
 
 
 
 void MOVE_PidRun(void)
 {
+	
 	move.M3508.LPf.Speed=0.8 * move.M3508.Rx.Speed + 0.2 * move.M3508.LPf.Speed;//滤波器
-  move.M3508.Output = pid_increment_update(move.M3508.TarSpeed, move.M3508.LPf.Speed, &move.M3508.PidSpeed);
+	move.M3508.Output = pid_increment_update(move.M3508.TarSpeed, move.M3508.LPf.Speed, &move.M3508.PidSpeed);
 	move.M3508.OutputLpf = 0.8 * move.M3508.Output + 0.2 * move.M3508.OutputLpf;//滤波器
 }
 
 void MOVE_CanTransmit(void)
 {
-    move.CanData[6]=(uint8_t)(move.M3508.OutputLpf>>8);
-    move.CanData[7]=(uint8_t)(move.M3508.OutputLpf);
-		CAN1_Transmit(0x200,move.CanData);
+	move.CanData[6]=(uint8_t)(move.M3508.OutputLpf>>8);
+	move.CanData[7]=(uint8_t)(move.M3508.OutputLpf);
+	CAN1_Transmit(0x200,move.CanData);
 }
 
-void MOVE_Process(RemoteData_t RDMsg)
+void MOVE_Process(void)
 {
-	
-    MOVE_GetMoveData();
-
-    MOVE_PidRun();
-
-    MOVE_CanTransmit();
+	MOVE_GetMoveData();
+	MOVE_PidRun();
+	MOVE_CanTransmit();
 }
+/*************************main function***************************/
 
+int16_t Tar_speed;
+_Bool color_id;
+void windmill_Process(void)
+{
+	 switch(queue_get())
+	 {
+		 case run_led:
+			 Run_led(); break;
+     case motor_stop:
+			 Tar_speed = SPEED3; break;
+		 case run_motor:
+			 Tar_speed = SPEED1; break;
+		 case orange_color:
+			 color_id = 1; break;
+		 case cyan_color:
+			 color_id = 0; break;
+		 case led_reset:
+			 Reset(); break;
+			 
+   }
+	 MOVE_Process();
+}
--- project_1/windmill.h
+++ project_1/windmill.h
@@ -3,6 +3,22 @@
 #include "pid.h"
 
 
+//ЦёБо
+#define led_reset 1
+#define run_led 2
+
+#define motor_stop 3
+#define run_motor 4
+
+#define orange_color 5
+#define cyan_color 6
+
+#define success_hit 7
+#define fail_hit 8
+
+#define movement_normal 9
+#define movement_other 10
+
 typedef struct _move_t
 {
     M3508_t M3508;
@@ -11,4 +27,10 @@ typedef struct _move_t
 }move_t;
 
 void MOVE_pidinit(void);
-
+void MOVE_Process(void);
+void RGB_LED_Write_24Bits(uint8_t green,uint8_t red,uint8_t blue);//GRB......
+void Run_led(void);
+uint8_t queue_get(void);
+extern int16_t Tar_speed;
+extern _Bool color_id;
+	
